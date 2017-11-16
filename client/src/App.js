e40ce8c8c6aa09e3113afd5df10f0dc114d78b91
Anthony Limere <anthonylimere@gmail.com>
2017-11-16 17:40:26 +0100

    feat: Add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "./components/Header.css";
 
 import Header from "./components/Header";
 
+const Home = () => {
+  return(
+    <div>home</div>
+  )
+}
+
+const NewSpot = () => {
+  return(
+    <div>new spot</div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/"    component={Home} />
+            <Route exact path="/add" component={NewSpot} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return(
     <div id="navbar">
-
+      <Link to="/" className="nav_button">
+        <p>Home</p>
+      </Link>
+      <Link to="/add" className="nav_button">
+        <p>ADD SPOT</p>
+      </Link>
     </div>
   )
 }
